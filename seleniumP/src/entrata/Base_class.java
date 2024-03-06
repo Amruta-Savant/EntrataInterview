1633a4df9948d09df472e7c2e9991f877f0337da
Amruta-Savant <162018353+Amruta-Savant@users.noreply.github.com>
2024-03-06 12:34:03 +0530

    Update Base_class.java

--- seleniumP/src/entrata/Base_class.java
+++ seleniumP/src/entrata/Base_class.java
@@ -7,15 +7,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Base_class {
